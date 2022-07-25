e5f06b78d94a2d1d4a39b74e54d4e17f8ebd3087
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-25 16:16:13 -0400

    added area total

--- Circle Objects/Program.cs
+++ Circle Objects/Program.cs
@@ -13,10 +13,13 @@
             Console.WriteLine();
 
             circles.Add(new Circle(102));
+            circles.Add(new Circle(202));
+            circles.Add(new Circle(10));
 
             Console.WriteLine();
             Console.WriteLine(lineBreak);
-            Console.WriteLine($"Godbye! You made {circles.Count} circles");
+            Console.WriteLine($"Godbye! You made {circles.Count} circles.");
+            Console.WriteLine($"With a total area of {Circle.FormatNumber(circles.Sum(n => n.CalculateArea()))}");
             Console.WriteLine(lineBreak);
         }
     }
