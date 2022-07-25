12fffb37cb34c32788415fd34364d346673b60b5
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-25 17:09:57 -0400

    Added lambda expressions

--- Circle Objects/Circle.cs
+++ Circle Objects/Circle.cs
@@ -26,46 +26,31 @@
         /// Caculates circle circumference
         /// </summary>
         /// <returns>The circumference of the circle</returns>
-        public double CalculateCircumference()
-        {
-            return radius * 2 * Math.PI;
-        }
+        public double CalculateCircumference() => radius * 2 * Math.PI;
 
         /// <summary>
         /// Formats the circles circumference
         /// </summary>
         /// <returns>The formated circumference rounded to the secound decimal place</returns>
-        public string CalculateFormattedCircumference()
-        {
-            return FormatNumber(CalculateCircumference());
-        }
+        public string CalculateFormattedCircumference() => FormatNumber(CalculateCircumference());
 
         /// <summary>
         /// Caculates the circles area
         /// </summary>
         /// <returns>The area of the circle</returns>
-        public double CalculateArea()
-        {
-            return radius * radius * Math.PI;
-        }
+        public double CalculateArea() => radius * radius * Math.PI;
 
         /// <summary>
         /// Formats the area of the circle 
         /// </summary>
         /// <returns>The area of the circle rounded to the second decimal place</returns>
-        public string CalculateFormattedArea()
-        {
-            return FormatNumber(CalculateArea());
-        }
+        public string CalculateFormattedArea() => FormatNumber(CalculateArea());
 
         /// <summary>
         /// Rounds the number to the second decimal place
         /// </summary>
         /// <param name="x"></param>
         /// <returns></returns>
-        public static string FormatNumber(double x)
-        {
-            return Math.Round(x, 2).ToString("N");
-        }
+        public static string FormatNumber(double x) => Math.Round(x, 2).ToString("N");
     }
 }
