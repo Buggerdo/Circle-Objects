70979e77e84972eb5bce5f94d960ae24c22da10c
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-25 15:53:02 -0400

    Added comments

--- Circle Objects/Circle.cs
+++ Circle Objects/Circle.cs
@@ -22,26 +22,47 @@
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Caculates circle circumference
+        /// </summary>
+        /// <returns>The circumference of the circle</returns>
         public double CalculateCircumference()
         {
             return radius * 2 * Math.PI;
         }
 
+        /// <summary>
+        /// Formats the circles circumference
+        /// </summary>
+        /// <returns>The formated circumference rounded to the secound decimal place</returns>
         public string CalculateFormattedCircumference()
         {
             return FormatNumber(CalculateCircumference());
         }
 
+        /// <summary>
+        /// Caculates the circles area
+        /// </summary>
+        /// <returns>The area of the circle</returns>
         public double CalculateArea()
         {
             return radius * radius * Math.PI;
         }
 
+        /// <summary>
+        /// Formats the area of the circle 
+        /// </summary>
+        /// <returns>The area of the circle rounded to the second decimal place</returns>
         public string CalculateFormattedArea()
         {
             return FormatNumber(CalculateArea());
         }
 
+        /// <summary>
+        /// Rounds the number to the second decimal place
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         private static string FormatNumber(double x)
         {
             return Math.Round(x , 2).ToString();
