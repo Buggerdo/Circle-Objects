c84280238df127b3538649605bdec2989d71977e
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-25 07:29:25 -0400

    Added Circle class

---
+++ Circle Objects/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Circle_Objects
+{
+    internal class Circle
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        [Required]
+        [Range(0, 10)]
+        public double Radius { get; private set; }
+    }
+}
