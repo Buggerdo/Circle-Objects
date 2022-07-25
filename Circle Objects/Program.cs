108a704b166b76e682e98e853874de1956860f17
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-25 13:44:26 -0400

    Added welcome and exit messages

--- Circle Objects/Program.cs
+++ Circle Objects/Program.cs
@@ -2,8 +2,22 @@
 {
     internal class Program
     {
+        public static string lineBreak = "====================================";
         static void Main()
         {
+            List<Circle> circles = new();
+
+            Console.WriteLine(lineBreak);
+            Console.WriteLine("Welcome to the circle creator");
+            Console.WriteLine(lineBreak);
+            Console.WriteLine();
+
+            circles.Add(new Circle(102));
+
+            Console.WriteLine();
+            Console.WriteLine(lineBreak);
+            Console.WriteLine($"Godbye! You made {circles.Count} circles");
+            Console.WriteLine(lineBreak);
         }
     }
 }
