4c60fb26d7bc6d884cfa16bac00fbf5697f3dd37
Troy <wilson.troy.anthony@gmail.com>
2022-07-25 21:38:19 -0400

    Added a user input method and a continue method

---
+++ Circle Objects/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+    internal class Validator
+    {
+        public static bool UserInput(out double x)
+        {
+            try
+            {
+                x = double.Parse(Console.ReadLine());
+                return true;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("That is not a valid input.");
+                x = 0;
+                return false;
+            }
+        }
+
+        public static bool Continue()
+        {
+            Console.Write("Continue Y/N? ");
+            string userInput = Console.ReadLine().ToLower().Trim();
+            if("yes".StartsWith(userInput))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
